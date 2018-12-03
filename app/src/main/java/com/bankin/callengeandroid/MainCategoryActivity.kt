e2fde7b4b1e29a78d381fb23360b5739d927479a
SocialSupaCrew <socialsupacrew@gmail.com>
2018-12-03 21:17:44 +0100

    Bootstrap project

---
+++ app/src/main/java/com/bankin/callengeandroid/MainCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.bankin.callengeandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainCategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_category)
+    }
+}
